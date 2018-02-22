4d3c6428d3c42e124cd29857b9e855c01358631a
Davy Cielen <davy.cielen@maitongroup.co.uk>
2018-02-22 16:02:42 +0100

    testing mysql connection

--- tests/testEnvVariables.py
+++ tests/testEnvVariables.py
@@ -1,5 +1,6 @@
 import nose
-
+import pymysql
+import pandas as pd
 import os
 
 # make sure you have copied the file with environmental variables into the directory and run the docker
@@ -9,7 +10,22 @@ LOCAL_INSTALL_DIR_ENV_FILES = os.path.join(os.getcwd() ,"config","envVars.env")
 
 def testEnvVariables2():
     """check that the environmental variables are set"""
+    """if these tests fail, make sure you have copied the envVars.env file,
+    this contains passswords and is stored outside the gitRepo"""
     assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_PASSWORD"]
     assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_DATABASE"]
     assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_USER"]
     assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_LINK"]
+
+def testMySQLConnection():
+    xUsername = os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_USER"]
+    xPassword = os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_PASSWORD"]
+    xDatabase = os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_DATABASE"]
+    xHost = os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_LINK"]
+    connection = pymysql.connect(user=xUsername,
+                                 password=xPassword,
+                                 database=xDatabase,
+                                 host=xHost)
+    assert pd.read_sql("select 1 as x", connection)["x"][0] == 1
+
+    #assert False
