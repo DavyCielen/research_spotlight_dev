436ef62f17f1b449ba9447c07e4da989b5948638
Davy Cielen <davy.cielen@maitongroup.co.uk>
2018-02-22 14:00:45 +0100

    test method to see if environment variables are passed into a container

--- tests/testEnvVariables.py
+++ tests/testEnvVariables.py
@@ -4,6 +4,12 @@ import os
 
 # make sure you have copied the file with environmental variables into the directory and run the docker
 
+LOCAL_INSTALL_DIR_ENV_FILES = os.path.join(os.getcwd() ,"config","envVars.env")
 
-def testEnvVariables():
-    assert(os.environ['testEnvVarsExists']) != None)
+
+def testEnvVariables2():
+    """check that the environmental variables are set"""
+    assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_PASSWORD"]
+    assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_DATABASE"]
+    assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_USER"]
+    assert os.environ["ERC_MYSQL_RESEARCH_HIGHLIGHTS_LINK"]
